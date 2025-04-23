283ecdfa3a1207694d091129d4941fdfe96fbad9
SamohtNB <60231047+SamohtNB@users.noreply.github.com>
2025-04-23 15:32:16 +0200

    finishing ex 2

--- build/unpack_to_raw.py
+++ build/unpack_to_raw.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import boto3
 
-def unpack_data(input_dir, output_file):
+def unpack_to_raw(input_dir, bucket_name, output_file_name):
     """
     Unpacks and combines multiple CSV files from a directory into a single CSV file.
 
@@ -13,12 +13,10 @@ def unpack_data(input_dir, output_file):
 
     # Step 1: Initialize an empty list to store DataFrames
     list_data = []
-    
-    s3 = boto3.client('s3', endpoint_url='https://localhost:4566', region_name='us-east-1')
+
     for root, dirs, files in os.walk(input_dir):
         print(f"Processing directory: {root}")
         for file in files:
-            print(f"Processing file: {file} | file path: {os.path.join(root, file)}")
             if file.endswith(".csv") or file.startswith("data-"):
                 
                 # Step 4: Read the CSV file using pandas
@@ -28,20 +26,39 @@ def unpack_data(input_dir, output_file):
                 
                 # Step 5: Append the DataFrame to the list
                 list_data.append(data_temp)
-        print("nexte directory\n")
     # Step 6: Concatenate all DataFrames
     if list_data:
         # Check if the list is not empty before concatenating
         print("Concatenating DataFrames...")
         data = pd.concat(list_data, axis=0, join="outer")
+        
+        tmp_dir = os.path.join("tmp")
+        os.makedirs(tmp_dir, exist_ok=True)
+        
+        combined_csv_path = os.path.join(tmp_dir, output_file_name)
+        data.to_csv(combined_csv_path, index=True)
+        print(f"Combined CSV file temporarly created at: {combined_csv_path}")
+        
+        s3 = boto3.client(
+            "s3",
+            endpoint_url="http://localhost:4566",  # ✅ PAS de HTTPS ici !
+            aws_access_key_id="root",
+            aws_secret_access_key="root",
+            region_name="us-east-1",
+            verify=False  # ✅ Ignore la vérification SSL
+        )
+        
+        s3.upload_file(
+            Filename=combined_csv_path,
+            Bucket=bucket_name,
+            Key=output_file_name
+        )
+                
+        print(f"Combined CSV file uploaded to S3 bucket '{bucket_name}' with name '{output_file_name}'")
     else:
         # If the list is empty, create an empty DataFrame
         print("No CSV files found in the directory.")
         return
-    
-    # Step 7: Save the combined DataFrame to output_file
-
-    data.to_csv(output_file)
 
 
 if __name__ == "__main__":
@@ -49,7 +66,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Unpack and combine protein data")
     parser.add_argument("--input_dir", type=str, required=True, help="Path to input directory")
-    parser.add_argument("--output_file", type=str, required=True, help="Path to output combined CSV file")
+    parser.add_argument("--bucket_name", type=str, required=True, help="Name of the S3 bucket")
+    parser.add_argument("--output_file_name", type=str, required=True, help="Path to output combined CSV file")
     args = parser.parse_args()
 
-    unpack_data(args.input_dir, args.output_file)
+    unpack_to_raw(args.input_dir, args.bucket_name, args.output_file_name)
